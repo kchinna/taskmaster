00f397aeed560fba33cb7eec4536906dba5215a4
kchinna <kanishkchinna@gmail.com>
2022-01-16 02:00:29 -0800

    changed title

--- src/Components/Sign-In/Sign-In.js
+++ src/Components/Sign-In/Sign-In.js
@@ -3,7 +3,7 @@ import React from "react";
 const Signin = (props) => {
   return (
     <div>
-      <h1>Task Master</h1>
+      <h1>TaskMaster</h1>
       <button onClick={props.signin}>Sign In</button>
       {/* <button onClick={firebaseSignOut}>Sign Out</button> */}
     </div>
